95abc758082a5b931ebc4582662ca178d18b692e
Arif0004 <arifmahmudaj6@gmail.com>
2022-06-14 12:38:04 +0600

    callback function

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function Calculate(num){
+    console.log(`${num} of squre is: ${num*num}`);
+}
+0
+function higherorderfunction(x, callback){
+    callback(x)
+}
+
+higherorderfunction(6, Calculate)
